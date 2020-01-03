5acd59eb0dfbde43b84412e6815f812ac77c3ca5
Shagun Madhikarmi <madhikarma@gmail.com>
2020-01-03 16:39:03 +0000

    Fixes initiialiser for model struct not being exposed as public for external modules

--- Sources/GitHubAPI/Models/GitHubSearchResult.swift
+++ Sources/GitHubAPI/Models/GitHubSearchResult.swift
@@ -14,4 +14,13 @@ public struct GitHubSearchResult: Identifiable, Codable {
     public let html_url: URL
     public let stargazers_count: Int
     public let watchers_count: Int
+    
+    public init(id: Int, full_name: String, description: String, html_url: URL, stargazers_count: Int, watchers_count: Int) {
+        self.id = id
+        self.full_name = full_name
+        self.description = description
+        self.html_url = html_url
+        self.stargazers_count = stargazers_count
+        self.watchers_count = watchers_count
+    }    
 }
