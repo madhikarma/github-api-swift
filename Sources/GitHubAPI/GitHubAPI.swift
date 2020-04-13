4d5a2c979041b131155a1faf71bc4569f5e714f8
Shagun Madhikarmi <madhikarma@gmail.com>
2020-04-13 19:27:01 +0100

    Add support for Combine framework via returning Publishers instead of closures

--- Sources/GitHubAPI/GitHubAPI.swift
+++ Sources/GitHubAPI/GitHubAPI.swift
@@ -5,29 +5,26 @@
 //  Created by Shagun Madhikarmi on 02/10/2019.
 //
 
+import Combine
 import Foundation
 
 public class GitHubAPI {
-    
-    private let session: URLSession
-    private let decoder: JSONDecoder
-    
-    
+    fileprivate let session: URLSession
+    fileprivate let decoder: JSONDecoder
+
     // MARK: - Initialisers
-    
+
     public init(session: URLSession = URLSession.shared, decoder: JSONDecoder = JSONDecoder()) {
         self.session = session
         self.decoder = decoder
     }
-    
-    
+
     // MARK: - Get Search Results
-    
+
     @discardableResult
-    public func getSearchResults(term: String, completion: @escaping (Result<[GitHubSearchResult], GitHubSearchResultError>) -> ()) -> URLSessionTask {
-        
-        let url = URL(string: "https://api.github.com/search/repositories?q=\(term)")!
-        let task = session.dataTask(with: url) { (data, response, urlError) in
+    public func getSearchResults(term: String, completion: @escaping (Result<[GitHubSearchResult], GitHubSearchResultError>) -> Void) -> URLSessionTask {
+        let url = makeSearchURL(term)
+        let task = session.dataTask(with: url) { data, _, urlError in
             let result = self.parseResponse(data: data, error: urlError)
             DispatchQueue.main.async {
                 completion(result)
@@ -36,29 +33,42 @@ public class GitHubAPI {
         task.resume()
         return task
     }
-    
-    
-    
-    // MARK: - Private
-    
-    private func parseResponse(data: Data?, error: Error?) -> Result<[GitHubSearchResult], GitHubSearchResultError> {
 
+    // MARK: - Private
+
+    fileprivate func parseResponse(data: Data?, error: Error?) -> Result<[GitHubSearchResult], GitHubSearchResultError> {
         if let urlError = error {
             return .failure(GitHubSearchResultError.unknown(urlError))
         }
-               
+
         guard let jsonData = data else {
             return .failure(GitHubSearchResultError.emptyData)
         }
-        
+
         do {
             let searchResponse = try decoder.decode(GitHubSearchResponse.self, from: jsonData)
             return .success(searchResponse.items)
-        } catch (let jsonError) {
+        } catch let jsonError {
             return .failure(GitHubSearchResultError.parsingData(jsonError))
         }
     }
 
+    fileprivate func makeSearchURL(_ term: String) -> URL {
+        return URL(string: "https://api.github.com/search/repositories?q=\(term)")!
+    }
 }
 
+extension GitHubAPI {
+    // MARK: - Combine
 
+    @available(OSX 10.15, *)
+    public func getSearchResults(_ term: String) -> AnyPublisher<GitHubSearchResponse, Error> {
+        let url = makeSearchURL(term)
+        let publisher = session.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .tryMap { (data) -> GitHubSearchResponse in
+                try self.decoder.decode(GitHubSearchResponse.self, from: data)
+            }.eraseToAnyPublisher()
+        return publisher
+    }
+}
--- Tests/GitHubAPITests/GitHubAPITests.swift
+++ Tests/GitHubAPITests/GitHubAPITests.swift
@@ -1,28 +1,58 @@
-import XCTest
+import Combine
 @testable import GitHubAPI
+import XCTest
 
+@available(OSX 10.15, *)
 final class GitHubAPITests: XCTestCase {
-    
+    private var cancellableSet: Set<AnyCancellable> = []
+
+    override func tearDown() {
+        super.tearDown()
+        cancellableSet.forEach { cancellable in
+            cancellable.cancel()
+        }
+    }
+
     func testGetSearchResults() {
-        
         // Given
         let gitHubAPI = GitHubAPI()
         let expectation = XCTestExpectation()
-        
+
         // When
         gitHubAPI.getSearchResults(term: "swift") { result in
             expectation.fulfill()
-            // Then            
+            // Then
             switch result {
-            case .success(let items):
+            case let .success(items):
                 print("search success: \(items)")
                 XCTAssertFalse(items.isEmpty)
-            case .failure(let error):
+            case let .failure(error):
                 XCTFail("Error: expected search results but instead there was an error: \(error)")
             }
-
         }
-        XCTWaiter().wait(for: [expectation], timeout: 5)
+        XCTWaiter().wait(for: [expectation], timeout: 1)
+    }
+
+    func testGetSearchResponse() {
+        // Given
+        let gitHubAPI = GitHubAPI()
+        let expectation = XCTestExpectation()
+
+        gitHubAPI.getSearchResults("swift")
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case let .failure(error):
+                        XCTFail("Error: expected search results but instead there was an error: \(error)")
+                    default:
+                        break
+                    }
+                }, receiveValue: { someValue in
+                    XCTAssertNotNil(someValue)
+                }
+            )
+            .store(in: &cancellableSet)
+        XCTWaiter().wait(for: [expectation], timeout: 1)
     }
 
     static var allTests = [
